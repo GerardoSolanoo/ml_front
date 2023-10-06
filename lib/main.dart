b711dc46078a5372534179139d52b7faa3b42488
GerardoSolanoo <gerardosolano.contact@gmail.com>
2023-10-06 08:38:48 -0600

    flutter disease

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ml_front/views/home_page.dart';
+import 'package:provider/provider.dart';
+
+import 'services/auth_service.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [ChangeNotifierProvider(create: (_) => AuthService())],
+        child: MaterialApp(
+            theme: ThemeData(primaryColor: Colors.green[900]),
+            debugShowCheckedModeBanner: false,
+            home: HomePage()));
+  }
+}
